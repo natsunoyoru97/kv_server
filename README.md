3eaa46e559b3a928c05af248e08e457bd8ff9d6f
natsunoyoru97 <honey970125@gmail.com>
2022-02-17 19:07:03 +0800

    A: Storage trait
    A: two examples: client, dummy_server

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # kv_server
 A simple structured key-value server in Rust.
+
+![server structure](./img/rfc793.webp)
+
---
+++ src/service/mod.rs
@@ -0,0 +1,19 @@
+
+
+/// 对 Command 的处理的抽象
+pub trait CommandService {
+    /// 处理 Command，返回 Response
+    fn execute(self, store: &impl Storage) -> CommandResponse;
+}
+
+
+/// 从 Request 中得到 Response，目前处理 HGET/HGETALL/HSET
+pub fn dispatch(cmd: CommandRequest, store: &impl Storage) -> CommandResponse {
+    match cmd.request_data {
+        Some(RequestData::Hget(param)) => param.execute(store),
+        Some(RequestData::Hgetall(param)) => param.execute(store),
+        Some(RequestData::Hset(param)) => param.execute(store),
+        None => KvError::InvalidCommand("Request has no data".to_owned()).into(),
+        _ => KvError::Internal("Not implemented".to_owned()).into(),
+    }
+}
